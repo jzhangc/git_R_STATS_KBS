6d026ba7c3bf98ba3759247a9dca8212bb33cef4
jzhangc <jzhangc@gmail.com>
2016-04-06 17:26:08 -0400

    functions nat_dvsr() and all_dvsr() added in prep for the upcoming 0.2.0 update

---
+++ package/frogplots/R/axisrange.R
@@ -0,0 +1,44 @@
+#' @title int_dvsr
+#'
+#' @description A simple to use function to find all divisors for an integer number
+#' @param x An integer number
+#' @return A vector containing all the divisors for the input integer
+#' @examples
+#' \dontrun{
+#' int_dvsr(10)
+#' }
+#' @export
+nat_dvsr<-function(x){
+tstSeq<-seq_len(abs(x))
+tstSeq[x%%tstSeq == 0] # %% does "modulus"
+}
+
+#' @title all_dvsr
+#'
+#' @description A function to find all divisors for any types of number
+#' @param x The number of interest
+#' @param i Decimal scaling factor. when \code{i>0}, if the input number is an integer, the starting number will be \code{x*10^i}; if a decimal, the starting number will be \code{x*10^n*10^i} (or \code{x*10^(n+i)}).
+#' @return A vector containing all the divisors for the input number
+#' @examples
+#' \dontrun{
+#' all_dvsr(0.5)
+#' all_dvsr(0.6,1) # with decimal scaling factor set at 1, meaning the divisor will based on 60
+#' all_dvsr(2)
+#' all_dvsr(2,1) # with decimal scaling factor set at 1, meaning the divisor will based on 20
+#' }
+#' @export
+all_dvsr<-function(x,i=0){
+  dopt<-getOption("scipen")
+  on.exit(options(scipen = dopt))
+
+  options(scipen=999)
+  if (x%%1==0){
+    smpl<-x*10^i
+    return(nat_dvsr(smpl)/10^i)
+  } else {
+    n<-nchar(unlist(strsplit(as.character(x),"[.]"))[2])
+    sc<-as.integer(round(x*10^(n+i)))
+    sc<-abs(sc)
+    return(nat_dvsr(sc)/(10^(n+i)))
+  }
+}
