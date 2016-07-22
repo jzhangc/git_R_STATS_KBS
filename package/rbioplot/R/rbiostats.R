6b26374db2024fb777216745685a4ce33ea75edd
Jing Zhang <jzhangc@Jings-Retina-MacBook-Pro.local>
2016-07-22 15:31:23 -0400

    0.2.5 heatmap added

--- package/rbioplot/R/rbiostats.R
+++ package/rbioplot/R/rbiostats.R
@@ -1,5 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.")
+  packageStartupMessage("Written by Jing Zhang, Ph.D. Please direct questions to jzhangcad@gmail.com.
+  For more details, please visit: http://kenstoreylab.com/?page_id=2448")
   return(TRUE)
 }
 
